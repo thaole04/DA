c670a0f128ffd53c8799477350bda94f8fd01863
Tuan Anh <anhtrantuan1208>
2025-04-11 03:29:15 +0700

    [TuanAnh] Improve pe.v

--- rtl/pe.v
+++ rtl/pe.v
@@ -8,60 +8,123 @@ module PE_conv_1x1 #(
     input  wire                      input_ready,
     input  wire [8*IN_CHANNEL-1:0]   input_data,   // unsigned [7:0]
     input  wire [8*IN_CHANNEL-1:0]   kernel_data,  // signed [7:0]
-    input  wire [15:0]               coeff,        // Q0.16 unsigned
+    input  wire [15:0]               coeff,        // Q1.16 unsigned
     input  wire [31:0]               bias,         // Q8.16 signed
-    output reg  [7:0]                output_data,  // unsigned 8-bit
-    output reg                       output_valid
+    output [7:0]                     output_data,  // unsigned 8-bit
+    output                           output_valid
 );
 
     // Output from macc
-    wire [15+$clog2(IN_CHANNEL):0] macc_out;
-    wire                           macc_valid;
+    localparam MACC_OUTPUT_DATA_WIDTH = 16 + $clog2(IN_CHANNEL);
+
+    wire [MACC_OUTPUT_DATA_WIDTH-1:0] macc_data_out;
+    wire                           macc_valid_o;
 
     // Instantiate MACC module (parallel mult + adder tree)
     macc_8bit_single #(
         .NUM_INPUTS(IN_CHANNEL)
     ) u_macc_8bit_single (
-        .o_data   (macc_out),
-        .o_valid  (macc_valid),
+        .o_data   (macc_data_out),
+        .o_valid  (macc_valid_o),
         .i_data_a (input_data),
         .i_data_b (kernel_data),
         .i_valid  (input_ready),
         .clk      (clk),
         .rst_n    (rst_n)
     );
-
-    // Internal pipeline registers
-    reg signed [47:0] scaled_result;
-    //reg signed [24:0] scaled_result;
-    reg signed [31:0] biased_result;
-    reg [7:0]         relu_result;
-    reg signed [31:0] biased_rounded;   
     
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            output_data  <= 8'd0;
-            output_valid <= 1'b0;
-        end else begin
-            if (macc_valid) begin
-                // Multiply by coeff (Q0.16)
-                scaled_result = $signed(macc_out) * coeff;
-
-                // Add bias (Q8.16)
-                biased_result = scaled_result[31:0] + bias;
-
-                // ReLU + Clamping
-                if (biased_result[23:0] < 0)
-                    relu_result = 8'd5;
-                else
-                    biased_rounded = biased_result + 32'h00008000;
-                    relu_result = (biased_result[23:16] > 8'd255) ? 8'd255 : biased_result[23:16];
-
-                output_data  <= relu_result;
-                output_valid <= 1'b1;
-            end else begin
-                output_valid <= 1'b0;
-            end
+    // MACC out reg
+    reg signed [MACC_OUTPUT_DATA_WIDTH-1:0] macc_data_out_reg;
+    reg                                     macc_valid_o_reg;
+    
+    always @ (posedge clk) begin
+        if (macc_valid_o) begin
+            macc_data_out_reg <= macc_data_out;
         end
     end
+
+    always @ (posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            macc_valid_o_reg <= 1'b0;
+        end
+        else begin
+            macc_valid_o_reg <= macc_valid_o;
+        end
+    end
+    
+    
+    // MACC co-eff
+    // Internal pipeline registers
+    reg signed [17+MACC_OUTPUT_DATA_WIDTH-1:0] coeff_prod;
+    //reg signed [24:0] coeff_prod;
+    reg                                        coeff_valid;   
+    wire signed [16:0] coeff_ext = {1'b0, coeff};
+    
+    always @(posedge clk or negedge rst_n) begin
+        if (macc_valid_o_reg) begin
+          // Multiply by coeff_ext (Q1.16)
+          coeff_prod = $signed(macc_data_out_reg) * coeff_ext;
+        end
+    end
+    
+    always @ (posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            coeff_valid <= 1'b0;
+        end
+        else begin
+            coeff_valid <= macc_valid_o_reg;
+        end
+    end
+    
+    // Bias
+    reg signed [17+MACC_OUTPUT_DATA_WIDTH-1:0] bias_sum;
+    //reg signed [31:0] bias_sum;
+    reg                                        bias_valid;
+    
+    always @ (posedge clk) begin
+        if (coeff_valid) begin
+            // Add bias (Q8.16)
+            bias_sum <= coeff_prod + bias;
+        end
+    end
+
+    always @ (posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            bias_valid <= 1'b0;
+        end
+        else begin
+            bias_valid <= coeff_valid;
+        end
+    end
+    
+    // Output
+    assign output_valid = bias_valid;
+    assign output_data  = bias_sum; //!
+    //always @(posedge clk or negedge rst_n) begin
+    //    if (!rst_n) begin
+    //        output_data  <= 8'd0;
+    //        output_valid <= 1'b0;
+    //    end else begin
+
+
+
+    //            // ReLU + Clamping
+    //            if (bias_sum[23:0] < 0)
+    //                relu_result = 8'd5;
+    //            else
+    //                biased_rounded = bias_sum + 32'h00008000;
+    //                relu_result = (bias_sum[23:16] > 8'd255) ? 8'd255 : bias_sum[23:16];
+
+    //            output_data  <= relu_result;
+    //            output_valid <= 1'b1;
+    //        end else begin
+    //            output_valid <= 1'b0;
+    //        end
+    //    end
+    //end
+    reg [7:0]         relu_result;
+    reg signed [31:0] biased_rounded;
+    
 endmodule
+
+
---
+++ rtl/testbench/PE_conv_1x1_tb.v
@@ -0,0 +1,100 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/09/2025 11:40:21 PM
+// Design Name: 
+// Module Name: PE_conv_1x1_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PE_conv_1x1_tb(
+
+    );
+    // Parameters
+    parameter IN_CHANNEL = 4; 
+
+    // Signals
+    reg                         clk;
+    reg                         rst_n;
+    reg                         input_ready;
+    reg [8*IN_CHANNEL-1:0]      input_data;
+    reg [8*IN_CHANNEL-1:0]      kernel_data;
+    reg [15:0]                  coeff;
+    reg [31:0]                  bias;
+    wire [7:0]                  output_data;
+    wire                        output_valid;
+
+    // Instantiate DUT
+    PE_conv_1x1 #(
+        .IN_CHANNEL(IN_CHANNEL)
+    ) dut (
+        .clk         (clk),
+        .rst_n       (rst_n),
+        .input_ready (input_ready),
+        .input_data  (input_data),
+        .kernel_data (kernel_data),
+        .coeff       (coeff),
+        .bias        (bias),
+        .output_data (output_data),
+        .output_valid(output_valid)
+    );
+
+    // Clock generation
+    initial clk = 0;
+    always #5 clk = ~clk; // 100MHz clock
+
+    // Reset
+    initial begin
+        rst_n = 0;
+        input_ready = 0;
+        input_data = 0;
+        kernel_data = 0;
+        coeff = 0;
+        bias = 0;
+
+        #20;
+        rst_n = 1;
+
+        #10;
+
+        // Set test values
+        @(posedge clk);
+        input_ready = 0;
+        
+        input_data = {
+            8'd255, 8'd255, 8'd255, 8'd255
+        }; // IN_CHANNEL = 4
+
+        kernel_data = {
+            $signed(-8'sd128), $signed(-8'sd128), $signed(-8'sd128), $signed(-8'sd128)
+        };
+
+        coeff = 16'h0100; // Q0.16 = 0.00390625
+        bias = 32'h00010000; // Q8.16 = 1.0
+        
+        @(posedge clk);
+        input_ready = 1;
+
+
+    
+        wait (output_valid);
+        $display("Output: %d", output_data);
+
+  
+        #50;
+        $finish;
+    end
+endmodule
